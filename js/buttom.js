5453dcb19ce7d4c65302bf5880bc7d14c9ee1758
颜浩轩 <yanhx23@mails.tsinghua.edu.cn>
2025-07-04 13:16:19 +0800

    个人经历框架基本搭好

--- js/buttom.js
+++ js/buttom.js
@@ -2,3 +2,10 @@ function toggleNavbar() {
         const navbar = document.querySelector('.navbar');
         navbar.classList.toggle('collapsed');
         }
+
+function toggleNavbar2() {
+        const container = document.querySelector('.container');
+        container.classList.toggle('co');
+        }
+
+        
